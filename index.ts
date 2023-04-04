94499a7b9bd6828a4bfc667029f405a93eaf3f27
eeroleppalehto <eero.leppalehto@gmail.com>
2023-04-04 14:47:48 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+import { Patient, Gender } from './types';
+import diagnoseService from './services/diagnoseService';
+
+const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3001;
+
+const patientList:Patient[] = [
+  {
+    id: '1',
+    name: 'Esko',
+    occupation: 'patient',
+    gender: Gender.Male
+  },
+  {
+    id: '2',
+    name: 'Asko',
+    occupation: 'undertaker',
+    gender: Gender.Other
+  }
+];
+
+app.get('/api/ping', (_req,res) => {
+  res.send('pong pat');
+});
+
+app.get('/api/patients', (_req, res) => {
+  res.json(patientList);
+});
+
+app.get('api/diagnoses', (_req, res) => {
+  console.log('hello');
+  res.send(diagnoseService.getAllDiagnoses());
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+});
---
+++ services/diagnoseService.ts
@@ -0,0 +1,8 @@
+import diagnoseEntries from "../data/diagnoses";
+import { Diagnosis } from "../types";
+
+const getAllDiagnoses = ():Diagnosis[] => {
+  return diagnoseEntries;
+};
+
+export default { getAllDiagnoses };
