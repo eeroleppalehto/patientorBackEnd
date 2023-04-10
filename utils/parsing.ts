efcf09250213c7de569b8f0df5daa43ae998d2b9
eeroleppalehto <eero.leppalehto@gmail.com>
2023-04-10 12:22:09 +0300

    Parsing for  req body for patient info

---
+++ utils/parsing.ts
@@ -0,0 +1,52 @@
+import { NewPatientEntry } from "../types";
+
+const toNewPatientEntry = (object: unknown): NewPatientEntry => {
+  if (!object || typeof object !== 'object') {
+    throw new Error('Incorrect or missing data');
+  }
+
+  if ( 'name' in object 
+    && 'occupation' in object 
+    && 'gender' in object 
+    && 'ssn' in object
+    && 'dateOfBirth' in object
+    ) {
+      const newPatient: NewPatientEntry = {
+        name: parseText(object.name),
+        occupation: parseText(object.occupation),
+        gender: parseText(object.gender),
+        ssn: parseText(object.ssn),
+        dateOfBirth: parseDate(object.dateOfBirth)
+      };
+
+      return newPatient;
+  }
+
+  throw new Error('Incorrect data: some fields are missing');
+};
+
+const parseText = (text: unknown): string => {
+  if (!isString(text)) {
+    throw new Error('Incorrect or missing field');
+  }
+
+  return text;
+};
+
+const parseDate = (date: unknown): string => {
+  if (!isString(date) || !isDate(date)) {
+    throw new Error('Invalid or missing date: ' + date);
+  }
+
+  return date;
+};
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+export default toNewPatientEntry;
