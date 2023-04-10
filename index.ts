5937954712cceda86a6c70f889ab0598320dea3f
eeroleppalehto <eero.leppalehto@gmail.com>
2023-04-10 12:20:40 +0300

    Added POST method for posting new patients entrys

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 //import { Patient, Gender } from './types';
 import diagnoseService from './services/diagnoseService';
 import patientService from './services/patientService';
+import toNewPatientEntry from './utils/parsing';
 
 const app = express();
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -11,32 +12,33 @@ app.use(express.json());
 
 const PORT = 3001;
 
-/* const patientList:Patient[] = [
-  {
-    id: '1',
-    name: 'Esko',
-    occupation: 'patient',
-    gender: Gender.Male
-  },
-  {
-    id: '2',
-    name: 'Asko',
-    occupation: 'undertaker',
-    gender: Gender.Other
-  }
-]; */
-
 app.get('/api/ping', (_req,res) => {
-  console.log('hello');
   res.send('pong pat');
 });
 
 app.get('/api/patients', (_req, res) => {
-  res.json(patientService.getAllPatients());
+  res.json(patientService.getAllNonSensitivePatients());
 });
 
 app.get('/api/diagnoses', (_req, res) => {
   res.json(diagnoseService.getAllDiagnoses());
+});
+
+app.post('/api/patients', (req, res) => {
+  try {
+    const newEntry = toNewPatientEntry(req.body);
+    const newPatient = patientService.addPatient(newEntry);
+
+    console.log('New patient added');
+    res.json(newPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong ';
+    if ( error instanceof Error) {
+      errorMessage += error.message;
+    }
+    console.log(errorMessage);
+    res.status(400).send(errorMessage);
+  }
 });
 
 app.listen(PORT, () => {
