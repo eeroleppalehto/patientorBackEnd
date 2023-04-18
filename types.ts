937d83041f55ec9db0418fd55547835cc7dbbe4b
eeroleppalehto <eero.leppalehto@gmail.com>
2023-04-18 12:52:52 +0300

    Entry types added

--- types.ts
+++ types.ts
@@ -10,8 +10,62 @@ export enum Gender {
   Other = "other"
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface BaseEntry {
+  id: string;
+  date: string;
+  description: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry = HealthCheckEntry | OccupationalHealthcareEntry | HospitalEntry;
+
+/* export interface Entry {
+  id: string;
+  date: string;
+  type: string;
+  specialist: string;
+  description: string;
+  diagnosisCodes?: string[];
+  discharge?: Discharge;
+  employerName?: string;
+  sickLeave?: SickLeave;
+  healthCheckRating?: number;
+}
+ */
 
 export interface Patient {
   id: string;
