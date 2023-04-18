883e4fc31092b6e1a83250a002e69bf0ebff2b11
eeroleppalehto <eero.leppalehto@gmail.com>
2023-04-18 12:53:24 +0300

    Typo

--- services/diagnoseService.ts
+++ services/diagnoseService.ts
@@ -1,7 +1,7 @@
 import diagnoseEntries from "../data/diagnoses";
 import { Diagnosis } from "../types";
 
-const getAllDiagnoses = ():Diagnosis[] => {
+const getAllDiagnoses = (): Diagnosis[] => {
   return diagnoseEntries;
 };
 
