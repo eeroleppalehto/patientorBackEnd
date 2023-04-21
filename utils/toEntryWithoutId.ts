934bcdd280131397fc9a3e11fcddc3a099866941
eeroleppalehto <eero.leppalehto@gmail.com>
2023-04-21 14:05:40 +0300

    bug fix

--- utils/toEntryWithoutId.ts
+++ utils/toEntryWithoutId.ts
@@ -82,12 +82,12 @@ const toEntryWithoutId = (object: unknown): EntryWithoutId => {
 
 
 const parseDiagnosisCodes = (object: unknown): Array<Diagnosis['code']> =>  {
-  if (!object || typeof object !== 'object' || !('diagnosisCodes' in object)) {
+  if (!object || typeof object !== 'object') {
     // we will just trust the data to be in correct form
     return [] as Array<Diagnosis['code']>;
   }
 
-  return object.diagnosisCodes as Array<Diagnosis['code']>;
+  return object as Array<Diagnosis['code']>;
 };
 
 const parseDischarge = (object: unknown): Discharge => {
